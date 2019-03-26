e6aeca274f5e6bca414fe3094bd990312b2be729
Danny Town <47186976+dannyotown@users.noreply.github.com>
2019-03-26 11:06:05 -0500

    Testing 3

---
+++ AddToAD/AddToAD/ActiveDirectoryAdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddToAD
+{
+    class ActiveDirectoryAdd
+    {
+        public struct UserInfo
+        {
+            //LDAP fields in Active Directory ---- You can add more here, such as "mail", "proxyAddresses", and "telephoneNumber"..
+            //sAMAccountName & username are required for creating a user.
+            public string username;  //UserName,  THIS IS REQUIRED
+            public string sAMAccountName; //SAMA account, THIS IS REQUIRED
+            public string givenName; //First Name
+            public string sn; //Last Name
+        }
+        private void CreateADUser(string FirstName, string LastName, uint PhoneNumber)
+        {
+            
+
+        }
+    }
+}
---
+++ AddToAD/AddToAD/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddToAD
+{
+    class Employee
+    {
+        public class EmployeeInfo
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public long PhoneNumber { get; set; }
+        }
+    }
+}
